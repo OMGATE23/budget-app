d6fc3d52e80dc5135076e48ef04b7f993a8bb854
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-18 15:12:44 +0530

    feat: created dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,72 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
-import { fetchData } from '../helper'
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
+import AddBudgetForm from "../components/AddBudgetForm";
+import Intro from "../components/Intro";
+import { createNewBudget, fetchData } from "../helper";
 
-export function dashboardLoader(){
-  const userName = fetchData("userName")
-  return {userName}
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  const budgets = fetchData("budgets");
+  return { userName, budgets };
+}
+
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  console.log("hi");
+  const actionType = data.get("_action")
+
+  if(actionType === "newUser"){
+    try {
+      const userName = data.get("_userName")
+      localStorage.setItem("userName", userName);
+      return toast.success(`Welcome, ${userName}!`);
+    } catch (err) {
+      return toast.error("Log in Failed");
+    }
+  }
+
+  if(actionType === "newBudget"){
+    console.log(data)
+    const name = data.get("newBudget")
+    const amount = data.get("newBudgetAmount")
+    console.log({name, amount})
+
+    try{
+      createNewBudget({name , amount})
+      return toast.success('Budget created')
+    } catch(err){
+      throw new Error("Uh oh! A problem occured while creating budget")
+    }
+
+
+  }
+  
 }
 const Dashboard = () => {
-  const {userName} = useLoaderData()
+  const { userName, budgets } = useLoaderData();
   return (
-    
-    <div>
-      <p>{userName}</p>
-      Dashboard: HI</div>
-  )
-}
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back, <span className="accent">{userName}</span>
+          </h1>
 
-export default Dashboard
+          <div className="grid-sm">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm/>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
+};
+
+export default Dashboard;
