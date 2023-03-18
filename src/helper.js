86813c7f072f6382598f91ea74c67c86a8d7936b
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-18 15:06:26 +0530

    feat: added create new budget function

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,26 @@
 export function fetchData(key){
     return localStorage.getItem(key)
 }
+
+export function deleteItem({key}){
+    return localStorage.removeItem(key)
+}
+
+export function createNewBudget({name , amount}){
+    const newItem = {
+        id : crypto.randomUUID(),
+        name : name,
+        createdAt : Date.now(),
+        amount : +amount,
+        color : generateRandomColor()
+    }
+
+    const existingBudgets = JSON.parse(fetchData('budgets')) ?? [];
+    return localStorage.setItem("budgets",
+    JSON.stringify([...existingBudgets, newItem]))
+}
+
+function generateRandomColor(){
+    let existingBudgetLength = JSON.parse(fetchData('budgets'))?.length ?? 0;
+    return `${existingBudgetLength * 34} 65% 50%`
+}
