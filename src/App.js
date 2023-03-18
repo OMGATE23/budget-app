cd1a9854db7d25699899b446a9d20ac577511e82
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-18 15:06:01 +0530

    feat: added routes and toast container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import { createBrowserRouter , RouterProvider } from "react-router-dom";
-import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import { logoutAction } from "./actions/logout";
+import Main, { mainLoader } from "./layouts/Main";
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const router = createBrowserRouter([
   {
     path : "/",
-    element : <Dashboard/>,
-    loader : dashboardLoader,
-  } , 
-
-  {
-    path : '/about',
-    element : <h1>About</h1>
-  }
+    element : <Main/>,
+    loader : mainLoader,
+    children : [
+      {
+        index : true,
+        element : <Dashboard/>,
+        loader : dashboardLoader,
+        action : dashboardAction,
+        
+        errorElement :<Error/>
+      } ,
+      {
+        path : "logout",
+        action : logoutAction
+      }
+    ]
+  },
+   
 ])
 function App() {
   return (
     <div className="App">
       <RouterProvider router = {router}/>
+      <ToastContainer/>
     </div>
   );
 }
