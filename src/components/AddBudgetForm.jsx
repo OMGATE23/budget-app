293c73b12286b6bb574cd28fcc193d6076a932d0
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-18 15:23:17 +0530

    feat: form clearing after submission

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,12 +1,24 @@
 import { CurrencyRupeeIcon } from "@heroicons/react/24/solid";
-import React from "react";
-import { Form } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Form, useFetcher } from "react-router-dom";
 const AddBudgetForm = () => {
+  const fetcher = useFetcher();
+  const isSubmitting = fetcher.state === "submitting";
+
+  const formRef = useRef();
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      formRef.current.reset();
+      inputRef.current.focus();
+    }
+  }, [isSubmitting]);
   return (
     <div className="form-wrapper">
       <h2 className="h3">Create budget</h2>
 
-      <Form method="post" className="grid-sm">
+      <fetcher.Form method="post" className="grid-sm" ref={formRef}>
         <div className="grid-xs">
           <label htmlFor="mewBudget">Budget Name</label>
           <input
@@ -14,27 +26,34 @@ const AddBudgetForm = () => {
             name="newBudget"
             id="newBudget"
             placeholder="e.g., Groceries"
+            ref={inputRef}
             required
           />
         </div>
-        <input type = "hidden" name = "_action" value = "newBudget"/>
-        <div className="grid-xs"> 
-            <label htmlFor="newBudgetAmount">Amount</label>
-            <input
-                type = "amount"
-                step = "0.01"
-                name = "newBudgetAmount"
-                id = "newBudgetAmount"
-                placeholder="Rs 2000"
-                required
-                inputMode="decimal"
-            />
+        <input type="hidden" name="_action" value="newBudget" />
+        <div className="grid-xs">
+          <label htmlFor="newBudgetAmount">Amount</label>
+          <input
+            type="amount"
+            step="0.01"
+            name="newBudgetAmount"
+            id="newBudgetAmount"
+            placeholder="Rs 2000"
+            required
+            inputMode="decimal"
+          />
         </div>
-        <button className="btn btn--dark">
-            <span>Create Budget</span>
-            <CurrencyRupeeIcon/>
+        <button className="btn btn--dark" disabled={isSubmitting}>
+          {isSubmitting ? (
+            <span>Creating Budget..</span>
+          ) : (
+            <>
+              <span>Create Budget</span>
+              <CurrencyRupeeIcon />
+            </>
+          )}
         </button>
-      </Form>
+      </fetcher.Form>
     </div>
   );
 };
