8f8efb8bcfa175a08fc0eb4c419e2247c7b301d9
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-03 19:15:20 +0530

    refactor : adjusted to delete items format

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -4,6 +4,8 @@ import { deleteItem } from "../helper";
 
 export function logoutAction(){
     deleteItem({key : "userName"})
+    deleteItem({key : "budgets"})
+    deleteItem({key : "expenses"})
     toast.success('Logout Successful')
     return redirect('/')
 }
